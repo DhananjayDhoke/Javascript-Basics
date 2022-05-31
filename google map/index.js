4c67311b2c51df807ceea6d85828428c7cfd14bd
DhananjayDhoke <dhananjaydhoke3@gmail.com>
2022-05-31 22:03:13 +0530

    googl map

---
+++ google map/index.js
@@ -0,0 +1,23 @@
+ function initMap(){
+    
+    let option =  {
+        center: {lat: 21.1458, lng: 79.0882},
+        zoom: 8
+      }
+  
+
+    map = new google.maps.Map(document.getElementById('map'),option);
+
+    // marker 
+   function Addmarker(location){
+    const marker = new google.maps.Marker({
+        position:location,
+        map: map,
+      });
+
+    }
+    Addmarker({lat:21.3929 ,lng:79.3268})
+    Addmarker({lat:18.5204 ,lng:73.8567})
+
+ }
+ 
